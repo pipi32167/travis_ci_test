f735115a3dfe0360bc9abec545a30090a532936c
杨其斌 <pipi32167@gmail.com>
2016-06-14 14:21:14 +0800

    add a failed test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
+'use strict';
 
+const assert = require('assert');
 
 describe('just a test', function() {
-  
+
   it('should test success', function() {
-    
+
+  });
+
+  it('should fail', function() {
+    assert.ok(false);
   });
 });
